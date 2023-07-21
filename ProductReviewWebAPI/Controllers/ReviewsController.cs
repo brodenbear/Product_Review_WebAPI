bfbdad7cb43ad9aa79df1db710b31d91b078d4e7
Broden Berrett <brodenb@gmail.com>
2023-07-20 22:45:26 -0600

    created ReviewsController and linked to DbContext

---
+++ ProductReviewWebAPI/Controllers/ReviewsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductReviewWebAPI.Data;
+
+namespace ProductReviewWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ReviewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Get()
+        {
+     
+        }
+    }
+}
